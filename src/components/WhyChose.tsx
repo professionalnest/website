182051504ecbcbe47939c080c431349ed687347a
akshitsingla <assl_dev@yahoo.com>
2025-02-11 07:40:05 +0000

    Fixed Why Choose Us panel

--- src/components/WhyChose.tsx
+++ src/components/WhyChose.tsx
@@ -3,38 +3,36 @@ export default function WhyChose() {
         {
             id: "1",
             name: "No Platform lock-in",
-            logo: "https://softr-assets-eu-shared.s3.eu-central-1.amazonaws.com/illustrations/rosa/people-working-in-a-informal-environment.svg",
+            image: "/illustrations/people-working-in-a-informal-environment.svg",
         },
         {
             id: "2",
             name: "Instant Access",
-            logo: "https://softr-assets-eu-shared.s3.eu-central-1.amazonaws.com/illustrations/rosa/people-chatting-around-the-office.svg",
+            image: "/illustrations/people-chatting-around-the-office.svg",
         },
         {
             id: "3",
             name: "Transparent Pricing",
-            logo: "https://assets.softr-files.com/applications/e9036e53-b096-4bc0-8e6c-d598c06d24be/assets/girl-with-laptop-F0F0F4.svg",
+            image: "/illustrations/girl-with-laptop-F0F0F4.svg",
         },
     ];
 
     return (
-        <section className="px-4 sm:px-12 py-8 sm:py-16 bg-white">
-            <h2 className="text-3xl sm:text-4xl font-bold text-center mb-8 sm:mb-12 text-black">
+        <section className="px-4 sm:px-12 py-8 sm:py-16">
+            <h2 className="text-3xl sm:text-4xl font-bold text-center mb-8 sm:mb-12">
                 Why choose ProNest?
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-12">
+            <div className="grid grid-cols-1 md:grid-cols-3 md:gap-8 sm:gap-12">
                 {whyChoose.map((item) => (
-                    <div key={item.id} className="flex flex-col items-center">
-                        <div className="relative w-48 h-48 sm:w-64 sm:h-64">
+                    <div key={item.id} className="flex flex-col items-center w-full p-4">
+                        <div className="">
                             <img
-                                src={item.logo}
-                                alt={`Feature illustration ${item.name}`}
-                                width={256}
-                                height={256}
+                                src={item.image}
+                                alt={`Illustration for ${item.name}`}
                                 className="object-contain w-full h-full"
                             />
                         </div>
-                        <h3 className="mt-4 text-xl font-semibold text-center text-gray-800">
+                        <h3 className="text-2xl font-semibold text-center text-gray-800">
                             {item.name}
                         </h3>
                     </div>
