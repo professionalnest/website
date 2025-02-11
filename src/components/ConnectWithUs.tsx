01bc2f8576166eb931f6831a9246e98c85664c03
akshitsingla <assl_dev@yahoo.com>
2025-02-11 12:34:12 +0000

    Fixing lint errors

--- src/components/ConnectWithUs.tsx
+++ src/components/ConnectWithUs.tsx
@@ -5,7 +5,7 @@ const ConnectWithUs = () => {
         <section className="w-fit px-4 py-20 mx-auto">
                 <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-8 lg:gap-20">
                     <div>
-                        <h2 className="text-3xl sm:text-4xl font-bold mb-3 sm:mb-4 text-black">Can't find what you are looking for?</h2>
+                        <h2 className="text-3xl sm:text-4xl font-bold mb-3 sm:mb-4 text-black">Can&apos;t find what you are looking for?</h2>
                   <p className="text-gray-600 mb-6 md:mb-0 text-sm sm:text-base">
                       If you cannot find the answer to your question in our FAQ, you can always contact us.
                   </p>
