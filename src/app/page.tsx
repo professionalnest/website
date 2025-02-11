f0c9e851a8199625c11d418bacb95e21cd8d1869
akshitsingla <assl_dev@yahoo.com>
2025-02-11 08:29:30 +0000

    Removed unused import in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import WhyChose from "@/components/WhyChoose";
 import  Hero  from "@/components/Hero";
 import ToolGrid  from "@/components/ToolGrid";
-import Footer  from "@/components/Footer";
 import BrowseDirectoryC2A from "@/components/BrowseDirectory";
 
 export default function Home() {
