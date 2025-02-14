b413a06b52dd309aecfc2bab9478ba492d246b55
Pankajkumar2608 <tarachandji104@gmail.com>
2025-02-14 19:54:26 +0530

    removed spaces

--- src/components/ContactUsInfo.tsx
+++ src/components/ContactUsInfo.tsx
@@ -9,7 +9,7 @@ const ContactUsInfo = () => {
             </div>
 
             <div>
-                <h2 className="text-2xl  sm:text-3xl font-bold mb-2 sm:mb-4">Follow Us</h2>
+                <h2 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">Follow Us</h2>
                 <a href="https://linkedin.com/company/professionalnest" className="flex flex-row w-fit mx-auto gap-1">
                     <Linkedin size={22} className="ml-3"/>
                     <span className="break-all">/professionalnest</span>
