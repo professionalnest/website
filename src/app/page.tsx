c34de92922555cddeb454dfb56bbfbba13ef8cd3
akshitsingla <assl_dev@yahoo.com>
2025-03-06 11:18:03 +0530

    Implemented new architecture for Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import WhyChose from "@/components/WhyChoose";
 import  Hero  from "@/components/Hero";
 import ToolGrid  from "@/components/ToolGrid";
 import BrowseDirectoryC2A from "@/components/BrowseDirectory";
+import { homePageLabels } from "@/utils/labels";
+import { HeroComponentLabels } from "@/models/UIInterfaces";
 
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <Hero labels={homePageLabels.hero} />
       <ToolGrid/>
       <WhyChose/>
       <BrowseDirectoryC2A />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { HeroComponentLabels } from "@/models/UIInterfaces";
 
-const Hero = () => {
+const Hero = ({labels}: {labels:HeroComponentLabels}) => {
   return (
     <div className="container m-auto flex flex-col md:flex-row items-center justify-between py-14 md:py-20 gap-14 md:px-0 px-12">
       {/* TEXT SIDE */}
@@ -10,17 +11,17 @@ const Hero = () => {
 
         {/* HERO TITLE */}
         <h1 className="text-4xl md:text-6xl font-bold leading-10 sm:leading-7"> 
-          Save Big on Your Favorite AI Tools
+          {labels.title}
         </h1>
 
         {/* HERO SUBTITLE */}
         <p className="text-2xl md:text-3xl text-gray-500 font-nunito">
-          Unlock huge discounts up to 50% off on the hottest artificial intelligence tools!
+          {labels.subtitle}
         </p>
 
         {/* CALL TO ACTION */}
         <div>
-        <Link href="/getstarted" className="px-6 md:px-8 py-4 bg-color-primary rounded-lg font-extrabold">Get Started</Link>
+        <Link href={labels.c2aLink} className="px-6 md:px-8 py-4 bg-color-primary rounded-lg font-extrabold">{labels.c2aText}</Link>
         </div>
       </div>
 
