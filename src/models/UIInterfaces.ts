3cd763915f4048996c9db15842875146aa973eb1
akshitsingla <assl_dev@yahoo.com>
2025-03-06 11:17:31 +0530

    Implemented labels architecture for UI elements.

---
+++ src/models/UIInterfaces.ts
@@ -0,0 +1,14 @@
+export interface HeroComponentLabels {
+    title: string,
+    subtitle: string,
+    c2aText: string,
+    c2aLink: string
+}
+
+export interface HomePageLabels {
+    hero: HeroComponentLabels
+}
+
+export interface AboutPageLabels {
+    description: string
+}
---
+++ src/utils/labels.ts
@@ -0,0 +1,15 @@
+import { AboutPageLabels, HomePageLabels } from "@/models/UIInterfaces";
+
+export const homePageLabels:HomePageLabels = {
+    "hero": {
+        "title": "Orchestrating Enterprise AI: From Chaos to Clarity",
+        "subtitle": "ProNest optimizes, secures, and streamlines AI investments for enterprises while empowering AI providers with robust sales and distribution channels.",
+        "c2aText": "Get Started",
+        "c2aLink": "/getstarted"
+    },
+
+};
+
+export const aboutPageLabels:AboutPageLabels = {
+    "description": "We help Enterprise AI buyers optimize, secure and secure their AI investments, and AI solution providers through sales and distribution solutions."
+};
