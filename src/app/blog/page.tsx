e4f00760f9b77aafb677a5861a6754c2f05cd7a5
akshitsingla <assl_dev@yahoo.com>
2025-03-10 00:13:43 +0530

    Also Commented featured post component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,17 @@
 import styles from "@/styles/blog.module.css";
-import { getBlogCategories, getBlogPostsSortedByDate, getFeaturedBlogPost } from "@/services/dataService";
-import FeaturedPost from "@/components/BlogFeaturedPost";
+import { getBlogCategories, getBlogPostsSortedByDate } from "@/services/dataService";
 import RecentPosts from "@/components/BlogRecentPosts";
 import BlogsList from "@/components/BlogsList";
-import SubscribeNewsletterC2A from "@/components/SubscribeNewsletterC2A";
 
 export default function Home() {
 
   const allBlogPosts = getBlogPostsSortedByDate();
-  const featuredBlogPost = getFeaturedBlogPost();
   const recentBlogPosts = allBlogPosts.slice(0, 3);
   const blogCategories = getBlogCategories();
 
   return (
     <main className={styles.main}>
-      {featuredBlogPost && <FeaturedPost blogPost={featuredBlogPost}/>}
+      {/* {featuredBlogPost && <FeaturedPost blogPost={featuredBlogPost}/>} */}
       
       <RecentPosts blogPosts={recentBlogPosts}/>
 
